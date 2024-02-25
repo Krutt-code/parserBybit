b7ebf79c891c678ebb133e3438306cf413a23400
Viktor <brykovvita173@gmail.com>
2024-02-26 00:39:07 +0300

    тест

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 from seleniumbase import Driver
+from time import sleep
 
 with Driver() as driver:
     driver.get('https://ploshadka.net/kak-obnovit-python-na-ubuntu/')
-    driver.implicitly_wait(10)
-    
+    # driver.implicitly_wait(10)
+    sleep(1)
+    print('Прошла 1с')
+    sleep(2)
     print(driver.find_element('h1').text)
